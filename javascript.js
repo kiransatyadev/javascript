49b5b6756106bbbe8c51060b094928e35086fb06
kiransatyadev <2000030473@kluniversity.in>
2024-05-26 05:22:06 +0530

    first commit

---
+++ javascript.js
@@ -0,0 +1,22 @@
+/* var a=5
+var b=9
+console.log(a+b)
+
+function fn() {
+    console.log("executed")
+}
+fn();
+var num=10
+if(num>0) {
+    console.log("posituve")
+}
+else if(num<0) {
+
+    console.log("negative")
+} */
+
+let fruits=["apple","grape","pineapple"];
+fruits.push("goa");
+fruits.pop();
+
+console.log(fruits)
